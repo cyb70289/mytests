b7e9d50667e82acf2445d6dc617969cfa22bf61d
Yibo Cai <cyb70289@gmail.com>
2019-02-03 13:02:42 +0800

    add memory order test (w/o barrier)
    caught on d05, centriq2400

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ all:
 
 .PHONY: clean
 clean:
-	rm -f read-le ascii 8way pagesize mt-print mt-cond
+	rm -f read-le ascii 8way pagesize mt-print mt-cond order
 
 ascii: ascii.c ascii.S
 	gcc ${CPPFLAGS} -o ascii $^
---
+++ order.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+const int consumers = 6;
+
+sem_t sem_consumer_prepare, sem_done_prepare;
+sem_t sem_consumer, sem_done;
+
+volatile char start_producer = 0;
+
+volatile char input[256];
+volatile char input_changed[256];
+
+volatile unsigned int caught = 0;
+
+char buf[128*1024];
+
+#define wmb()   __asm__ __volatile__ ("dmb ish"   : : : "memory")
+#define rmb()   __asm__ __volatile__ ("dmb ishld" : : : "memory")
+
+void fillcache()
+{
+    for (int i = 0; i < sizeof(buf); ++i)
+        buf[i] += i;
+}
+
+void *consumer(void *arg)
+{
+    while (1) {
+        sem_wait(&sem_consumer_prepare);
+        *input = 0x55;
+        *input_changed = 0;
+        sem_post(&sem_done_prepare);
+
+        sem_wait(&sem_consumer);
+
+        fillcache();
+
+        while (*input_changed == 0);
+        if (*input == 0x55) {
+            printf("caught!!!\n");
+            ++caught;
+        }
+
+        sem_post(&sem_done);
+    }
+
+    return NULL;
+}
+
+int main(void)
+{
+    unsigned int cnt = 0;
+    pthread_t tid[consumers];
+
+    srand(time(NULL));
+
+    sem_init(&sem_consumer_prepare, 0, 0);
+    sem_init(&sem_done_prepare, 0, 0);
+    sem_init(&sem_consumer, 0, 0);
+    sem_init(&sem_done, 0, 0);
+
+    for (int i = 0; i < consumers; ++i)
+        pthread_create(&tid[i], NULL, consumer, NULL);
+
+    while (1) {
+        for (int i = 0; i < consumers; ++i)
+            sem_post(&sem_consumer_prepare);
+
+        for (int i = 0; i < consumers; ++i)
+            sem_wait(&sem_done_prepare);
+
+        for (int i = 0; i < consumers; ++i)
+            sem_post(&sem_consumer);
+
+        fillcache();
+
+        *input = 0xAA;
+        *input_changed = 1;
+
+        for (int i = 0; i < consumers; ++i)
+            sem_wait(&sem_done);
+
+        ++cnt;
+        if (cnt % 1000 == 0)
+            printf("%u:%u \n", cnt, caught);
+    }
+
+    return 0;
+}
