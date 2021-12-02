437833357a5f2a00cddfc129ee040697e70b39cf
Yibo Cai <cyb70289@gmail.com>
2021-12-02 15:25:15 +0800

    cppmem: fix multi-copy-atomicity test

--- cppmem/multi-copy.c
+++ cppmem/multi-copy.c
@@ -4,8 +4,8 @@ int main() {
 
   {{{ { x.store(1); }
   ||| { y.store(1); }
-  ||| { x.load().readsvalue(0); y.load().readsvalue(1); }
   ||| { x.load().readsvalue(1); y.load().readsvalue(0); }
+  ||| { y.load().readsvalue(1); x.load().readsvalue(0); }
   }}};
   return 0;
 }
