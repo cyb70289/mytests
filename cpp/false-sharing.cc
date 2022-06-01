a63630671b4b399d800ec4ec2de4789d484cef48
Yibo Cai <cyb70289@gmail.com>
2022-06-01 13:36:32 +0800

    cpp: update false-sharing.cc

--- cpp/false-sharing.cc
+++ cpp/false-sharing.cc
@@ -1,19 +1,22 @@
-#include <atomic>
 #include <thread>
 
 struct alignas(64) {
-    std::atomic<int> a;
-#if 0
-    char pad[60];
-#endif
-    std::atomic<int> b;
+    volatile int a;
+    //char pad[60];  // force a, b in different cacheline
+    volatile int b;
 } g;
 
 const int n = 12345678;
 
 int main() {
-    std::thread t([]() { for (int i = 0; i < n; ++i) ++g.a; });
-    for (int i = 0; i < n; ++i) ++g.b;
+    // not atomic
+    //std::thread t([]() { for (int i = 0; i < n; ++i) ++g.a; });
+    //for (int i = 0; i < n; ++i) ++g.b;
+
+    // atomic
+    std::thread t([]() { for (int i = 0; i < n; ++i) __atomic_add_fetch(&g.a, 1, __ATOMIC_RELAXED); });
+    for (int i = 0; i < n; ++i) __atomic_add_fetch(&g.b, 1, __ATOMIC_RELAXED);
+
     t.join();
     return 0;
 }
