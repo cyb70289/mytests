1ad8a3cfb89592e06f8a7e05975102524c577697
Yibo Cai <cyb70289@gmail.com>
2023-04-11 15:40:58 +0800

    mysql: add aio to mydd.cc

--- mysql/io-bench/mydd.cc
+++ mysql/io-bench/mydd.cc
@@ -1,6 +1,4 @@
-// to experience nvme queue usage
-// - buffer io: queue used effectively by nvme driver
-// - direct io: queue not effectively used
+// to evaluate nvme queue usage under direct-io and async-io
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -8,43 +6,23 @@
 #include <strings.h>
 #include <unistd.h>
 
+#include <linux/aio_abi.h>
+#include <sys/syscall.h>
+
 #include <thread>
 #include <vector>
 
 const long block_size = 8 * 1024;
 const long block_count = 1024 * 1024;
-alignas(4096) char iobuf[block_size]{};
+alignas(4096) char iobuf[block_size];
 
-// Evaluate if nvme queues are used effectively
-// Usage:
-// - ./mydd direct 32    --> direct-io with 32 threads
-// - ./mydd              --> buffered-io (1 thread)
-int main(int argc, char *argv[]) {
-    int o_direct = 0;
-    int n_threads = 1;
-#ifdef O_DIRECT
-    if (argc > 1 && strcasecmp(argv[1], "direct") == 0) {
-        o_direct = O_DIRECT;
-        printf("enable direct io\n");
-        if (argc == 3) {
-            n_threads = atoi(argv[2]);
-            if (n_threads <= 0) n_threads = 1;
-        }
-        printf("threads = %d\n", n_threads);
-    }
-#endif
-
-    int fd = open("./__test__.bin", O_CREAT | O_TRUNC | O_WRONLY | o_direct,
-                  0644);
-    if (fd < 0) { perror("open"); return 1; }
-
-    /* Though each thread writes its own non-overlapped region, for direct-io,
-     * looks nvme driver only uses one queue.
-     *
-     * fd: |--------------|--------------|--------------|------------
-     *     ^  thd0 write  ^  thd1 write  ^  thd2 write  ^ ....
-     */
-
+/* Though each thread writes its own non-overlapped region, for direct-io,
+ * nvme driver only uses one queue.
+ *
+ * fd: |--------------|--------------|--------------|------------
+ *     ^  thd0 write  ^  thd1 write  ^  thd2 write  ^ ....
+ */
+static void evaluate_dio(int fd, int n_threads) {
     std::vector<std::thread> threads;
     for (int i = 0; i < n_threads; ++i) {
         threads.emplace_back(
@@ -62,6 +40,98 @@ int main(int argc, char *argv[]) {
     }
     for (int i = 0; i < n_threads; ++i) {
         threads[i].join();
+    }
+}
+
+static int io_setup(unsigned nr, aio_context_t *ctxp) {
+    return syscall(__NR_io_setup, nr, ctxp);
+}
+
+static int io_destroy(aio_context_t ctx) {
+    return syscall(__NR_io_destroy, ctx);
+}
+
+static int io_submit(aio_context_t ctx, long nr, struct iocb **iocbpp) {
+    return syscall(__NR_io_submit, ctx, nr, iocbpp);
+}
+
+static int io_getevents(aio_context_t ctx, long min_nr, long max_nr,
+                        struct io_event *events, struct timespec *timeout) {
+    return syscall(__NR_io_getevents, ctx, min_nr, max_nr, events, timeout);
+}
+
+static void evaluate_aio(int fd, int depth) {
+    aio_context_t aio_ctx{};
+    if (io_setup(depth, &aio_ctx)) {
+        perror("io_setup");
+        exit(1);
+    }
+
+    struct iocb cb{};
+    struct iocb *cbs[1]{&cb};
+    cb.aio_fildes = fd;
+    cb.aio_lio_opcode = IOCB_CMD_PWRITE;
+    cb.aio_buf = reinterpret_cast<uintptr_t>(iobuf);
+    cb.aio_nbytes = block_size;
+
+    int pending_io = 0;
+    long sent_blocks = 0;
+    while (sent_blocks < block_count) {
+        if (pending_io < depth && (sent_blocks + pending_io) < block_count) {
+            // queue one io
+            cb.aio_offset = sent_blocks * block_size;
+            if (io_submit(aio_ctx, 1, cbs) != 1) {
+                perror("io_submit");
+                exit(1);
+            }
+            ++pending_io;
+        } else {
+            // wait for one io done
+            struct io_event events[1];
+            if (io_getevents(aio_ctx, 1, 1, events, nullptr) != 1) {
+                perror("io_getevents");
+                exit(1);
+            }
+            --pending_io;
+            ++sent_blocks;
+        }
+    }
+
+    io_destroy(aio_ctx);
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < block_size; ++i) {
+        iobuf[i] = char(i);
+    }
+
+    bool aio = false;
+    int depth = 1;
+
+    if (argc >= 2) {
+        depth = atoi(argv[1]);
+        if (depth <= 0) depth = 1;
+        if (argc == 3 && strcasecmp(argv[2], "aio") == 0) {
+            aio = true;
+            printf("AIO+DIO, queue depth = %d\n", depth);
+        } else {
+            printf("DIO, threads = %d\n", depth);
+        }
+    } else {
+        printf("Usage:\n");
+        printf("  ./mydd 32             direct-io with 32 threads\n");
+        printf("  ./mydd 16 aio         aio + dio, queue depth = 16\n");
+        return 1;
+    }
+
+    int fd = open("./__test__.bin", O_CREAT | O_TRUNC | O_WRONLY | O_DIRECT,
+                  0644);
+    if (fd < 0) { perror("open"); return 1; }
+
+    if (aio) {
+        evaluate_aio(fd, depth);
+    } else {
+        evaluate_dio(fd, depth);
     }
 
     close(fd);
