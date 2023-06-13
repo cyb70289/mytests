539ab6e6add834206c98e517b70491608d5cc373
Yibo Cai <cyb70289@gmail.com>
2023-06-13 10:40:57 +0800

    mysql: add memory benchmark

---
+++ mysql/io-bench/memory.cc
@@ -0,0 +1,59 @@
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <random>
+
+#pragma GCC optimize ("align-functions=65536")
+
+constexpr int64_t N = 1 * 1024 * 1024 * 1024;
+constexpr int64_t L = 100 * 1000 * 1000;
+
+__attribute__((noinline))
+uint32_t get5(const int64_t* index, const uint8_t* data, int64_t i) {
+  return data[index[i]];
+}
+
+__attribute__((noinline))
+uint32_t get4(const int64_t* index, const uint8_t* data, int64_t i) {
+  return get5(index, data, i);
+}
+
+__attribute__((noinline))
+uint32_t get3(const int64_t* index, const uint8_t* data, int64_t i) {
+  return get4(index, data, i);
+}
+
+__attribute__((noinline))
+uint32_t get2(const int64_t* index, const uint8_t* data, int64_t i) {
+  return get3(index, data, i);
+}
+
+__attribute__((noinline))
+uint32_t get1(const int64_t* index, const uint8_t* data, int64_t i) {
+  return get2(index, data, i);
+}
+
+int main() {
+  auto data = std::make_unique<uint8_t[]>(N);
+  for (int64_t i = 0; i < N; ++i) {
+    data[i] = i;
+  }
+
+  std::mt19937 mt(42);
+  std::uniform_int_distribution<int64_t> dist(0, N-1);
+  auto index = std::make_unique<int64_t[]>(L);
+  for (int64_t i = 0; i < L; ++i) {
+    index[i] = dist(mt);
+  }
+
+  uint32_t s = 0;
+  const auto t1 = std::chrono::high_resolution_clock::now();
+  for (int64_t i = 0; i < L; ++i) {
+    s += get1(index.get(), data.get(), i);
+  }
+  const auto t2 = std::chrono::high_resolution_clock::now();
+  const std::chrono::duration<double> elapsed = t2 - t1;
+  std::cout << "read : " << elapsed.count() << " s\n";
+
+  return int(s);
+}
